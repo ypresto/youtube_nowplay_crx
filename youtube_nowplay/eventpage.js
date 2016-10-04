2c80a48a73ea1287ff09a78231e97df6809fe530
Yuya Tanaka <yuya.presto@gmail.com>
2016-10-05 03:06:33 +0900

    Implement click to focus feature

--- youtube_nowplay/eventpage.js
+++ youtube_nowplay/eventpage.js
@@ -4,24 +4,35 @@
 const DURATION = 5000
 let lastNotificationId = null
 let lastTimerId = null
+let senderTabId = null
+let senderWindowId = null
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   if (lastNotificationId) {
     chrome.notifications.clear(lastNotificationId)
   }
   if (lastTimerId !== null) {
     clearTimeout(lastTimerId)
   }
+  senderTabId = sender.tab.id
+  senderWindowId = sender.tab.windowId
 
   // TODO: Set imageUrl to thumbnail of video.
   chrome.notifications.create({
     type: "basic",
     iconUrl: chrome.runtime.getURL('youtube.png'),
     title: 'Now Playing (YouTube)',
-    message: request.currentTitle
+    message: message.currentTitle,
+    isClickable: true
   }, notificationId => lastNotificationId = notificationId)
 
   lastTimerId = setTimeout(function() {
     chrome.notifications.clear(lastNotificationId)
   }, DURATION)
+})
+
+chrome.notifications.onClicked.addListener(function(notificationId) {
+  chrome.notifications.clear(notificationId)
+  chrome.tabs.update(senderTabId, {selected: true})
+  chrome.windows.update(senderWindowId, {focused: true})
 })
